d05bb7aa04c04ec0d5a7309ee57df4afdc23e30c
yanleiXzgh <119163095+yanleiXzgh@users.noreply.github.com>
2024-07-04 17:40:52 +0800

    Update EqualSearch.py

--- EqualSearch.py
+++ EqualSearch.py
@@ -1,16 +1,14 @@
-def equal_search(file_path:str,target_data:str,target_element:int) -> str:
-
+def equal_search(file_path: str, target_data: str, target_element: int) -> str:
     with open(file_path, 'r') as file:
         data_read = file.readlines()
-        print('data read',data_read)
-       
+        print('data read', data_read)
+
         data_list2 = []
         for i in data_read:
             data_list1 = i.split('.')
-            data_list1[len(data_list1)-1] = data_list1[len(data_list1)-1].strip()
+            data_list1[len(data_list1) - 1] = data_list1[len(data_list1) - 1].strip()
             data_list2.append(data_list1)
-            print('data_list2',data_list2)
-
+            print('data_list2', data_list2)
     for j in data_list2:
         for k in j:
             if k == target_data:
@@ -18,7 +16,8 @@ def equal_search(file_path:str,target_data:str,target_element:int) -> str:
 
                 return target_list[target_element]
 
+
 def data_write(data_write):
-    with open('./mylist','w') as file:
+    with open('./mylist', 'w') as file:
         file.write(data_write)
 
